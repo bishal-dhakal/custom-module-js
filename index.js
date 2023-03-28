ee3872c60af1ecfb0976bb136454d81b353e30e8
bishal <46195146+bishal-dhakal@users.noreply.github.com>
2023-03-28 20:31:06 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// const math = require('./more')
+
+// console.log(math);
+// console.log(math.sub(5,5));
+// console.log(math.add(5,10));
+
+
+//object destructuring
+const {add , sub } = require('./more');
+
+console.log(add(5,5));
